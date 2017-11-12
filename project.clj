1334738c9f45effbd1f97ab54af030357bafc1ad
Zaphod Beeblebrox <alexguev@gmail.com>
2017-11-11 23:29:12 -0500

    use patched version of gorilla-repl

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.4.0"
+(defproject lein-gorilla "0.4.0-zb0th"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.4.0"]]
+  :dependencies [[gorilla-repl "0.4.0-zb0th"]]
   :eval-in-leiningen true)
